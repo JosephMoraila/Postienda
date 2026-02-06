ebee69148c029e9cdbde8c183323552858f0a7c5
JoseAndresMoraila <eljugadorfeliz@gmail.com>
2026-02-06 15:07:27 -0700

    [FIX] Path and BOM

--- constants/JSON/Session/Session.cpp
+++ constants/JSON/Session/Session.cpp
@@ -1,4 +1,9 @@
 ﻿#include "constants/JSON/Session/Session.h"
 #include "constants/FOLDERS/SETTINGS/SETTINGS_FOLDER.hpp"
 
-const std::string JSON_SESSION_PATH = SETTINGS_FOLDER_PATH() + "session.json"; // Ruta al json de sesiÃÂ³n
+#ifdef _WIN64
+const std::string JSON_SESSION_PATH = SETTINGS_FOLDER_PATH() + "\\" + "session.json"; // Ruta al json de sesiÃÂ³n
+#elif defined(__linux__)
+const std::string JSON_SESSION_PATH = SETTINGS_FOLDER_PATH() + "/" + "session.json";
+#endif
+
